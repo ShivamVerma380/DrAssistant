560410a0f52093ba60826fbeaf242e03e95bea6d
Shivam Verma <shivam.r.verma@seagate.com>
2022-11-27 19:20:27 +0530

    Working on signup
    Signed-off-by: Shivam Verma <shivam.r.verma@seagate.com>

--- app/src/main/java/com/shivam/texttospeech/signup/SignUpActivity.java
+++ app/src/main/java/com/shivam/texttospeech/signup/SignUpActivity.java
@@ -3,14 +3,30 @@ package com.shivam.texttospeech.signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.shivam.texttospeech.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText userName,password,sex,address,phoneNo,bloodGroup,height,weight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        userName = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+        sex = findViewById(R.id.sex);
+        address = findViewById(R.id.address);
+        phoneNo = findViewById(R.id.phoneNumber);
+        bloodGroup = findViewById(R.id.bloodGroup);
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+
+
+
+
     }
 }
