ebfcae7f24fd68145be914879d015ba4defec8b6
Shivam Verma <shivam.r.verma@seagate.com>
2022-11-27 18:54:21 +0530

    Integrated login api
    Signed-off-by: Shivam Verma <shivam.r.verma@seagate.com>

--- app/src/main/java/com/shivam/texttospeech/signup/LoginActivity.java
+++ app/src/main/java/com/shivam/texttospeech/signup/LoginActivity.java
@@ -5,14 +5,108 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.shivam.texttospeech.R;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email,password;
+
+    private Button login;
+
+    private ProgressBar progressBar;
+
+    private TextView txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.idEdtUserName);
+        password = findViewById(R.id.idEdtPassword);
+        progressBar = findViewById(R.id.progress_circular);
+        txtSignUp = findViewById(R.id.newUserSignUp);
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        login = findViewById(R.id.idBtnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please enter correct email id or password",Toast.LENGTH_SHORT).show();
+                }else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    OkHttpClient client = new OkHttpClient();
+
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("email",email.getText().toString())
+                            .add("password",password.getText().toString())
+                            .build();
+
+                    Request request = new Request.Builder()
+                            .url(getString(R.string.api)+"/login-user")
+                            .post(requestBody)
+                            .build();
+
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                            LoginActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            if(response.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"Login Successfull",Toast.LENGTH_SHORT).show();
+
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                            }
+                            LoginActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    });
+                }
+
+
+            }
+        });
+
     }
     public void userSignUpPage(View v){
 //        Intent intent = new Intent(this, OtherActivity.class);
