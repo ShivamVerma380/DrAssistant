8bdcbcd4113f574dfb76aa0f69a87901fa40aaa2
unknown <karanrg3005@gmail.com>
2022-11-27 18:28:36 +0530

    login ui page created

--- app/src/main/java/com/shivam/texttospeech/auth/LoginActivity.java
+++ app/src/main/java/com/shivam/texttospeech/auth/LoginActivity.java
@@ -2,7 +2,10 @@ package com.shivam.texttospeech.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.shivam.texttospeech.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void userSignUpPage(View v){
+//        Intent intent = new Intent(this, OtherActivity.class);
+//        startActivity(intent);
     }
 }
